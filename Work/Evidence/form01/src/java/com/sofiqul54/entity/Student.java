b375aee063446d31491ac4fb12fb3516c8637c34
sofiqul-54 <sofiqul54@gmail.com>
2019-01-08 23:26:52 +0600

    merged

---
+++ Work/Evidence/form01/src/java/com/sofiqul54/entity/Student.java
@@ -0,0 +1,70 @@
+
+package com.sofiqul54.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private String gender;
+    private String round;
+    private String email;
+    private String password;
+    private String hobby;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getRound() {
+        return round;
+    }
+
+    public void setRound(String round) {
+        this.round = round;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+    
+    
+}
